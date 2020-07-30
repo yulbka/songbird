ccc24bf8e451873907686c8497ad7c4290038a3f
romanovskayaya <yulia.romanovskaya@oxagile.com>
2020-07-30 14:39:32 +0300

    feat: add rewind to audio player

--- src/components/Bar/Bar.jsx
+++ src/components/Bar/Bar.jsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
+
+import moment from 'moment';
+// eslint-disable-next-line no-unused-vars
+import momentDurationFormatSetup from 'moment-duration-format';
 
 import styles from './Bar.module.scss';
 
-export function Bar({ duration }) {
-  const curPercentage = 20;
+export function Bar({ currentTime, duration, updateTime }) {
+  const refProgress = useRef(null);
+
+  const curPercentage = currentTime / duration * 100;
+
+  const formatDuration = (duration) => {
+    return moment.duration(duration, 'seconds').format('mm:ss', { trim: false });
+  }
+
+  function calcTime(e) {
+    const barWidth = refProgress.current.offsetWidth;
+    const barStartCoord = refProgress.current.getBoundingClientRect().left;
+    const mousePositionInBar = e.clientX - barStartCoord;
+    const curPercentage = mousePositionInBar / barWidth;
+    return curPercentage * duration;
+  }
+
+  function changeAudioTime(e) {    
+    const time = calcTime(e);
+    updateTime(time);
+    const updateTimeOnMouseMove = (e) => {
+      const time = calcTime(e);
+      updateTime(time);
+    }
+
+    document.addEventListener('mousemove', updateTimeOnMouseMove);
+    document.addEventListener('mouseup', () => {
+      document.removeEventListener('mousemove', updateTimeOnMouseMove)
+    });
+  }
+
   return (
     <div className={styles.bar}>
       <div
@@ -12,18 +45,22 @@ export function Bar({ duration }) {
         style={{
           background: `linear-gradient(to right, #00bc8c ${curPercentage}%, #737373 0)`
         }}
+        ref={refProgress}
+        onMouseDown={(event) => changeAudioTime(event)}
       >
         <span className={styles.circle} style={{ left: `${curPercentage - 2}%` }}
         />
       </div>
       <div className={styles.info}>
-        <span className={styles.time}>00:00</span>
-      <span className={styles.time}>{duration}</span>
+        <span className={styles.time}>{formatDuration(currentTime)}</span>
+        <span className={styles.time}>{formatDuration(duration)}</span>
       </div>
     </div>
   )
 }
 
 Bar.propTypes = {
-  duration: PropTypes.number
+  currentTime: PropTypes.number,
+  duration: PropTypes.number,
+  updateTime: PropTypes.func
 }
--- src/containers/Player/Player.jsx
+++ src/containers/Player/Player.jsx
@@ -13,20 +13,28 @@ export class Player extends React.Component {
 
   constructor(props) {
     super(props);
+    
     this.state = {
       isPlaying: false,
+      currentTime: 0,
       audioDuration: 0
     };
 
     this.audioRef = React.createRef();
+    this.playAudio = this.playAudio.bind(this);
+    this.pauseAudio = this.pauseAudio.bind(this);
+    this.setAudioDuration = this.setAudioDuration.bind(this);
+    this.updateTime = this.updateTime.bind(this);
   }
 
   componentDidMount() {
-    this.audioRef.current.addEventListener('ended', () => this.setState({ isPlaying: false }));
+    this.audioRef.current.addEventListener('ended', this.pauseAudio);
   }
 
   componentWillUnmount() {
-    this.audioRef.current.removeEventListener('ended', () => this.setState({ isPlaying: false }));  
+    this.audioRef.current.removeEventListener('ended', this.pauseAudio);
+    this.audioRef.current.removeEventListener('loadedMetadata', this.setAudioDuration);
+    this.audioRef.current.removeEventListener('timeUpdate', this.updateTime);
   }
 
   playAudio() {
@@ -40,24 +48,34 @@ export class Player extends React.Component {
   }
 
   setAudioDuration() {
-    this.setState({ audioDuration: this.audioRef.current.duration })
+    this.setState({ audioDuration: this.audioRef.current.duration });
+  }
+
+  updateTime() {
+    this.setState({currentTime: this.audioRef.current.currentTime});
+  }
+
+  updateTimeOnMouseClick(time) {
+    this.audioRef.current.currentTime = time;
+    this.setState({currentTime: time});
   }
 
   render() {
 
-    const { isPlaying, audioDuration } = this.state;
+    const { isPlaying, currentTime, audioDuration } = this.state;
 
     return (      
       <div className={styles.player}>
-        <div className={styles.play} onClick={isPlaying ? this.pauseAudio.bind(this): this.playAudio.bind(this)} >
+        <div className={styles.play} onClick={isPlaying ? this.pauseAudio: this.playAudio} >
           <SVG src={isPlaying ? iconPause: iconPlay} width={18} height={18}/>
           <audio
             ref={this.audioRef}
             src={this.props.audioSrc}
-            onLoadedMetadata={() => this.setAudioDuration()}
+            onLoadedMetadata={this.setAudioDuration}
+            onTimeUpdate={this.updateTime}
           />
         </div>
-        <Bar duration={audioDuration} />           
+        <Bar duration={audioDuration} currentTime={currentTime} updateTime={this.updateTimeOnMouseClick.bind(this)}/>           
       </div>
     )
   }  
